661b2234eeeb3ce64417c30713ef5f133e39ae43
Dima <dr.trem@list.ru>
2016-11-26 12:13:51 +0200

    7 commit

--- Author.rb
+++ Author.rb
@@ -1,7 +1,11 @@
 class Author
   attr_accessor :name, :biography
 
-  def initialize(name: nil, biography: nil)
+  def initialize(name, biography, *all)
     @name, @biography = name, biography
+  end
+
+  def to_s
+    [@name, @biography].join(',')
   end
 end
