ab5831988651f68ef883343e8712b8521ada88a9
Dima <dr.trem@list.ru>
2016-11-29 10:04:05 +0200

    last commit

--- Book.rb
+++ Book.rb
@@ -1,7 +1,11 @@
 class Book
-	attr_accessor :title, :author
-	
-	def initialize(title: nil, author: nil)
-		@title, @author = title, author
-	end
+  attr_accessor :title, :author
+
+  def initialize(title, author, *all)
+    @title, @author = title, author
+  end
+
+  def to_s
+    [@title, @author].join(',')
+  end
 end
